54b5870e01b824399fbf17151c40fb80faf502f1
maciejlaufer <maciek.laufer@gmail.com>
2018-09-23 14:25:07 +0200

    Added new widgets and basic file structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
+import './screens/home.dart';
 
-void main(){
-  runApp(
-    Center(child: Text(
-      "Hello Flutter!",
-      textDirection: TextDirection.ltr
-    ),)
-  );
+void main() => runApp(new JustCycleApp());
+
+class JustCycleApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: "Just cycle",
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text("Just cycle title"),
+          ),
+          body: Home(),
+        ));
+  }
 }
