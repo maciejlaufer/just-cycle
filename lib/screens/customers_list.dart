a836541bbf51ac52c97cd94f98a5a67934b044e0
maciejlaufer <maciek.laufer@gmail.com>
2018-12-29 12:46:45 +0100

    Added getting data from rest api

--- lib/screens/customers_list.dart
+++ lib/screens/customers_list.dart
@@ -1,7 +1,12 @@
+import 'dart:async';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import '../globals.dart' as globals;
+import 'loader.dart';
 
 class CustomersList extends StatefulWidget {
-  final String shopId;
+  final int shopId;
   
   const CustomersList({ 
     Key key, 
@@ -12,19 +17,52 @@ class CustomersList extends StatefulWidget {
 }
 
 class _CustomersListState extends State<CustomersList> {
-  String shopId = null;
+  int shopId = null;
+  String shopName = '';
+  bool _isLoadingCustomers = true;
+  List customersList = [];
+
+  String url = 'http://192.168.1.11:61331/api/stores/' + globals.currentShopId.toString() + '/customers';
+
+  Future<String> getCustomerData() async {
+    var res = await http.get(
+      Uri.encodeFull(url), 
+      headers: { 
+        'Authorization': 'Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJhZG1pbiIsInVuaXF1ZV9uYW1lIjoiYWRtaW4iLCJpc3MiOiJodHRwOi8vbG9jYWxob3N0OjYxMzMxIiwiaWF0IjoxNTQ1OTgyNzY2LCJuYmYiOjE1NDU5ODI3NjYsImV4cCI6MTU0NjI0MTk2NiwianRpIjoiMDBiZjU0MTZkNmM4NDYyNjgwNjM4OGY4NjZhZGJiZjMifQ.Y43xCNQStRkQZExAOu18fRR4-1lxMlOsKRBpNAOU-d8hjd7Bpl6BAXhaH-M6aYHCiA_kWmfGiMfbJrhULXMcr3i7hoc4qKxmnDlQjUm_lEvKEBnUo7yt6boGd1ZmLxf3M41P1wYLAS4tP_W7wlELioI0C7WFQmAR_8lC_zoo_BXpSSaF80kqtpgoNRYTaxEM13QMRC2Ohy5iA2UTppFYwfEyFUusDJWDWLPrQy02YvtX4-aOzhY4Fmthbwa2-0shmOyhE7SV4oDKvG30_bt9HZsjkpkI3VH0ritje81fw2XlJty8NAUOTDgxe2UkbBFV0X842fv6e7mZZPzueBmlMA',
+        'Accept': 'aplication/json',
+      }
+    );
+    if (res == null || res.statusCode != 200)
+        throw new Exception('HTTP request failed, statusCode: ${res?.statusCode}, $url');
+    setState(() { 
+      customersList = json.decode(res.body);
+      _isLoadingCustomers = false;
+    });
+    return 'Success!';
+  }
+
+  Future<Null> _refresh() async {
+    await getCustomerData();
+    return null;
+  }
+
   @override
   initState() {
     super.initState();
     this.shopId = widget.shopId;
+    this.getCustomerData().then((success){
+
+    });
   }
-  String url = 'http://localhost:61331/api/';
-  String shopName = '';
+
+  final GlobalKey<RefreshIndicatorState> _refreshIndicatorKey =
+    new GlobalKey<RefreshIndicatorState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Delivery cost calculator'),
+        title: Text(globals.currentShopName),
         backgroundColor: Colors.redAccent
       ),
       body: Container(
@@ -33,7 +71,7 @@ class _CustomersListState extends State<CustomersList> {
           children: <Widget>[
             TextField(
               decoration: InputDecoration(
-                hintText: 'Search customers... ' + this.shopId
+                hintText: 'Search customers... '
               ),
               onChanged: (String newShopName){
                 setState((){
@@ -41,7 +79,40 @@ class _CustomersListState extends State<CustomersList> {
                 });
               },
             ),
-            Text(shopName)
+            Expanded(
+              child: Container(
+                padding: EdgeInsets.only(top: 20.0),
+                child: _isLoadingCustomers ? Center(child: FlipLoader()) : RefreshIndicator(
+                  key: _refreshIndicatorKey,
+                  onRefresh: _refresh,
+                  child: ListView.builder(
+                  itemCount: this.customersList == null ? 0 : this.customersList.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return Container(
+                      child: Center(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.stretch,
+                          children: <Widget>[
+                            GestureDetector(
+                              child: Card(
+                                child: Container(
+                                  child: ListTile(
+                                    title: Text(customersList[index]['fullName'] == null ? 'brak' : customersList[index]['fullName']),
+                                  )
+                                ),
+                              ),
+                              onTap: (){
+                                
+                              }
+                            )
+                          ],
+                        )
+                      )
+                    );
+                  }),
+                )
+              )
+            )
           ],
         )
       )
--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -4,6 +4,8 @@ import './customers_list.dart';
 import './app_drawer.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import '../globals.dart' as globals;
+import 'loader.dart';
 
 class HomePage extends StatefulWidget {
   var cameras;
@@ -12,27 +14,51 @@ class HomePage extends StatefulWidget {
 
   @override
   State<StatefulWidget> createState() => _HomePageState();
+
 }
 
 class _HomePageState extends State<HomePage> {
-  final String url = 'https://jsonplaceholder.typicode.com/comments';
-  List shopData = null;
+  final String url = 'http://192.168.1.11:61331/api/stores';
+  List shopData = [];
+  List searchList = [];
+  bool _isLoadingShops = true;
 
   Future<String> getShopData() async {
-    var res = await http.get(Uri.encodeFull(url), headers: { 'Accept': 'aplication/json'});
-    print(res.body);
+    var res = await http.get(
+      Uri.encodeFull(url), 
+      headers: { 
+        'Authorization': 'Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJhZG1pbiIsInVuaXF1ZV9uYW1lIjoiYWRtaW4iLCJpc3MiOiJodHRwOi8vbG9jYWxob3N0OjYxMzMxIiwiaWF0IjoxNTQ1OTgyNzY2LCJuYmYiOjE1NDU5ODI3NjYsImV4cCI6MTU0NjI0MTk2NiwianRpIjoiMDBiZjU0MTZkNmM4NDYyNjgwNjM4OGY4NjZhZGJiZjMifQ.Y43xCNQStRkQZExAOu18fRR4-1lxMlOsKRBpNAOU-d8hjd7Bpl6BAXhaH-M6aYHCiA_kWmfGiMfbJrhULXMcr3i7hoc4qKxmnDlQjUm_lEvKEBnUo7yt6boGd1ZmLxf3M41P1wYLAS4tP_W7wlELioI0C7WFQmAR_8lC_zoo_BXpSSaF80kqtpgoNRYTaxEM13QMRC2Ohy5iA2UTppFYwfEyFUusDJWDWLPrQy02YvtX4-aOzhY4Fmthbwa2-0shmOyhE7SV4oDKvG30_bt9HZsjkpkI3VH0ritje81fw2XlJty8NAUOTDgxe2UkbBFV0X842fv6e7mZZPzueBmlMA',
+        'Accept': 'aplication/json',
+      }
+    );
+    if (res == null || res.statusCode != 200)
+        throw new Exception('HTTP request failed, statusCode: ${res?.statusCode}, $url');
+    //print(res.body);
     setState(() {
       shopData = json.decode(res.body);
+      searchList = json.decode(res.body);
+      _isLoadingShops = false;
     });
     return 'Success!';
+  }
+
+  Future<Null> _refresh() async {
+    await getShopData();
+    return null;
   }
 
   @override
   initState() {
     super.initState();
-    this.getShopData();
+    this.getShopData().then((success){
+      globals.currentShopName = this.shopData[0]['name'];
+      globals.currentShopId = this.shopData[0]['id'];
+    });
   }
   
+  final GlobalKey<RefreshIndicatorState> _refreshIndicatorKey =
+    new GlobalKey<RefreshIndicatorState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -41,7 +67,10 @@ class _HomePageState extends State<HomePage> {
             title: Text('Just cycle'),
             backgroundColor: Colors.redAccent,
           ),
-          body: ListView.builder(
+          body: _isLoadingShops ? Center(child: FlipLoader()) : RefreshIndicator(
+            key: _refreshIndicatorKey,
+            onRefresh: _refresh,
+            child: ListView.builder(
             itemCount: this.shopData == null ? 0 : this.shopData.length,
             itemBuilder: (BuildContext context, int index) {
               return Container(
@@ -52,21 +81,26 @@ class _HomePageState extends State<HomePage> {
                       GestureDetector(
                         child: Card(
                           child: Container(
-                            padding: EdgeInsets.all(15.0),
-                            child: Text(shopData[index]['name']),
+                            child: ListTile(
+                              title: Text(searchList[index]['name']),
+                              trailing: searchList[index]['id'] == globals.currentShopId 
+                              ? Icon(Icons.check_box)
+                              : Icon(Icons.check_box_outline_blank)
+                            )
                           ),
                         ),
                         onTap: (){
-                          final snackBar = SnackBar(content: Text(shopData[index]['id'].toString()));
-                          Scaffold.of(context).showSnackBar(snackBar);
+                          globals.currentShopName = this.searchList[index]['name'];
+                          globals.currentShopId = this.searchList[index]['id'];
+                          setState(() {});
                         }
                       )
                     ],
                   )
                 )
               );
-            }
-          ),
+            }),
+          )
         );
   }
 }
